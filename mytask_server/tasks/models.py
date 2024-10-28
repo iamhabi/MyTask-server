16649bdcb1d62234b32607e5f535bceaa77cb674
iamhabi <github.unzip957@slmail.me>
2024-10-28 11:23:25 +0900

    DateField -> DateTimeField

--- mytask_server/tasks/models.py
+++ mytask_server/tasks/models.py
@@ -10,8 +10,8 @@ class Task(models.Model):
     user = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='tasks', related_query_name='tasks')
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    due_date = models.DateField(blank=True, null=True)
-    created = models.DateField(auto_now_add=True)
+    due_date = models.DateTimeField(blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
     
     def __str__(self) -> str:
         return self.title
