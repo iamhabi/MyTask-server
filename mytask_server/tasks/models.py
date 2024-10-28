4a95e26c33a5b32db03d140bee201006b6cb565a
iamhabi <github.unzip957@slmail.me>
2024-10-28 14:08:00 +0900

    Task as parent

--- mytask_server/tasks/models.py
+++ mytask_server/tasks/models.py
@@ -8,6 +8,7 @@ import uuid
 class Task(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='tasks', related_query_name='tasks')
+    parent = models.ForeignKey("self", on_delete=models.CASCADE, blank=True, null=True)
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     due_date = models.DateTimeField(blank=True, null=True)
