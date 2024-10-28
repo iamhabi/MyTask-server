7487bfb651625ea7ade26dc5126adb785985967f
iamhabi <github.unzip957@slmail.me>
2024-10-28 11:20:41 +0900

    add description, due_date

--- mytask_server/tasks/models.py
+++ mytask_server/tasks/models.py
@@ -9,6 +9,8 @@ class Task(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='tasks', related_query_name='tasks')
     title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    due_date = models.DateField(blank=True, null=True)
     created = models.DateField(auto_now_add=True)
     
     def __str__(self) -> str:
