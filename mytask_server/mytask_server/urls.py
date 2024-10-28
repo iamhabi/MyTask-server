c15c194098a2628a0edc3845f9979267f0272f2d
iamhabi <github.unzip957@slmail.me>
2024-10-28 11:11:32 +0900

    tasks

--- mytask_server/mytask_server/urls.py
+++ mytask_server/mytask_server/urls.py
@@ -15,15 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenRefreshView
 
 from accounts.views import RegisterView, CustomTokenObtainPairView
 
+from tasks.views import TaskViewSet
+
+router = DefaultRouter()
+router.register(r'tasks', TaskViewSet, basename='tasks')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/', include(router.urls)),
     path('api/register/', RegisterView.as_view(), name='register'),
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
--- mytask_server/tasks/models.py
+++ mytask_server/tasks/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from accounts.models import MyUser
+
+import uuid
+
+
+class Task(models.Model):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user = models.ForeignKey(MyUser, on_delete=models.CASCADE, related_name='tasks', related_query_name='tasks')
+    title = models.CharField(max_length=255)
+    created = models.DateField(auto_now_add=True)
+    
+    def __str__(self) -> str:
+        return self.title
